2742a7c0a64313ea498ed036847de90e2a4f40b1
Ilmar <ilmar.ilves44@gmail.com>
2024-02-01 16:52:59 +0200

    Added package &  added additional code logic

--- index.js
+++ index.js
@@ -1,11 +1,46 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const MessagingResponse = require('twilio').twiml.MessagingResponse;
+
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.post('/sms', (req, res) => {
+  const twiml = new MessagingResponse();
+
+  // Get user's message
+  const userMessage = req.body.Body.toLowerCase();
+
+  // Initialize user session if not exists
+  let userSession = {};
+  if (!req.session.userSession) {
+    req.session.userSession = {};
+  }
+  userSession = req.session.userSession;
+
+  // Chatbot logic based on user's message
+  if (!userSession.name) {
+    // If user has not provided name yet
+    userSession.name = userMessage;
+    twiml.message(`Hi ${userSession.name}! Please provide your address.`);
+  } else if (!userSession.address) {
+    // If user has provided name but not address
+    userSession.address = userMessage;
+    twiml.message(`Thanks, ${userSession.name}! Which internet package are you interested in?`);
+  } else if (!userSession.package) {
+    // If user has provided name and address but not the desired package
+    userSession.package = userMessage;
+    twiml.message(`Great, ${userSession.name}! We will process your request for ${userSession.package} at ${userSession.address}.`);
+    // Reset user session for the next interaction
+    req.session.userSession = {};
+  }
+
+  res.writeHead(200, { 'Content-Type': 'text/xml' });
+  res.end(twiml.toString());
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`Chatbot listening at http://localhost:${port}`);
 });
